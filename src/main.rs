7259864a985354bb8fc7602e893df23a649ce8fc
jeelpatel231 <jeelpatel231@gmail.com>
2025-10-21 02:21:22 +0530

    print binary packets from websocket

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod http;
 use crate::http::handshake::handle_http_upgrade;
 use anyhow;
 use bytes::BytesMut;
+use fastwebsockets::{OpCode, WebSocket};
+use nom::AsBytes;
 use tokio::net::{TcpListener, TcpStream};
 
 const NETWORK_BUFFER: usize = 2048;
@@ -9,7 +11,27 @@ const NETWORK_BUFFER: usize = 2048;
 async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
     let mut buffer = BytesMut::with_capacity(NETWORK_BUFFER);
     handle_http_upgrade(&mut stream, &mut buffer).await?;
-    // TODO: hand off to some websocket frame handling library after handshake is performed
+    handle_as_websocket(stream).await?;
+    Ok(())
+}
+
+async fn handle_as_websocket(stream: TcpStream) -> anyhow::Result<()> {
+    let mut ws = WebSocket::after_handshake(stream, fastwebsockets::Role::Server);
+    ws.set_writev(true);
+    ws.set_auto_close(true);
+    ws.set_auto_pong(true);
+
+    loop {
+        let frame = ws.read_frame().await?;
+        match frame.opcode {
+            OpCode::Close => break,
+            OpCode::Binary => {
+                print!("{}", std::str::from_utf8(frame.payload.as_bytes()).unwrap())
+            }
+            _ => {}
+        }
+    }
+
     Ok(())
 }
 
