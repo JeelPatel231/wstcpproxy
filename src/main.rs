7e181f976d506962000236e9fefee99c8d21b2b9
jeelpatel231 <jeelpatel231@gmail.com>
2025-10-24 22:05:17 +0530

    migrate to hyper

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,30 @@
-mod http;
-use crate::http::handshake::handle_http_upgrade;
 use anyhow::{self, bail};
-use bytes::BytesMut;
-use fastwebsockets::{FragmentCollector, Frame, OpCode, WebSocket};
-use nom::AsBytes;
+use fastwebsockets::{FragmentCollector, Frame, OpCode};
+use fastwebsockets::{Payload, upgrade};
+use http_body_util::Empty;
+use hyper::{
+    Request, Response,
+    body::{Bytes, Incoming},
+};
+use hyper_util::rt::TokioIo;
 use tokio::{
     io::{self, AsyncWriteExt},
-    net::{TcpListener, TcpStream, UnixStream},
+    net::{TcpListener, TcpStream},
 };
-use wstcpproxy::debug_print;
+use zerocopy::IntoBytes;
 
 const NETWORK_BUFFER: usize = 8192;
 
-async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
-    let mut buffer = BytesMut::with_capacity(NETWORK_BUFFER);
-    handle_http_upgrade(&mut stream, &mut buffer).await?;
-    // MISTAKE: bytes after header are already consumed and live inside the [buffer]. 
-    // those wont be received by handle_as_websocket function.
-    handle_as_websocket(stream).await?;
-    Ok(())
-}
-
-async fn handle_as_websocket(stream: TcpStream) -> anyhow::Result<()> {
-    let mut ws = WebSocket::after_handshake(stream, fastwebsockets::Role::Server);
+async fn handle_as_websocket(fut: upgrade::UpgradeFut) -> anyhow::Result<()> {
+    let mut ws = fut.await?;
     ws.set_writev(true);
     ws.set_auto_close(true);
     ws.set_auto_pong(true);
     let mut ws = FragmentCollector::new(ws);
 
-    let tcp_conn = TcpStream::connect("localhost:5900").await?;
-    // let tcp_conn = UnixStream::connect("/tmp/vnc_socket").await?;
+    let tcp_conn = TcpStream::connect("0.0.0.0:9000").await?;
 
-    println!("Connected!");
+    eprintln!("Connected!");
 
     let (reader, mut writer) = tcp_conn.into_split();
 
@@ -40,16 +33,11 @@ async fn handle_as_websocket(stream: TcpStream) -> anyhow::Result<()> {
     loop {
         tokio::select! {
             ws_frame_data = ws.read_frame() => {
-                debug_print!("WS WAS SELCTED");
                 let frame = ws_frame_data?;
                 match frame.opcode {
-                    OpCode::Close => {
-                        debug_print!("Close was recvd");
-                        break
-                    },
+                    OpCode::Close => bail!("Client closed connection."),
                     OpCode::Binary => {
                         let payload = frame.payload.as_bytes();
-                        debug_print!(&payload);
                         writer.write(payload).await?;
                         writer.flush().await?;
                     }
@@ -57,15 +45,15 @@ async fn handle_as_websocket(stream: TcpStream) -> anyhow::Result<()> {
                 }
             },
             _ = reader.readable() => {
-                debug_print!("Ready for Reading from socket!");
                 match reader.try_read(&mut con_buf) {
                     Ok(0) => bail!("Stream closed amidst reading"),
                     Ok(n) => {
                         let new = &con_buf[..n];
-                        let ws_frame = Frame::new(true, OpCode::Binary, None, fastwebsockets::Payload::Borrowed(new));
+                        let ws_frame = Frame::new(true, OpCode::Binary, None, Payload::Borrowed(new));
                         ws.write_frame(ws_frame).await?;
                     }
                     Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
+                        // skip iteration and allow event loop to continue
                         continue;
                     }
                     Err(e) => bail!(e),
@@ -74,18 +62,45 @@ async fn handle_as_websocket(stream: TcpStream) -> anyhow::Result<()> {
             }
         }
     }
-
-    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:8000").await?;
-
     println!("Listening on 8000");
 
+    let mut http = hyper::server::conn::http1::Builder::new();
+
     loop {
-        let (stream, _) = listener.accept().await?;
-        let _ = tokio::spawn(handle_connection(stream));
+        http.keep_alive(true);
+
+        loop {
+            let (stream, _) = listener.accept().await?;
+
+            let connection = http
+                .serve_connection(
+                    TokioIo::new(stream),
+                    hyper::service::service_fn(server_upgrade),
+                )
+                .with_upgrades();
+
+            tokio::spawn(async move {
+                if let Err(err) = connection.await {
+                    println!("Error serving HTTP connection: {err:?}");
+                }
+            });
+        }
     }
+}
+
+async fn server_upgrade(mut req: Request<Incoming>) -> anyhow::Result<Response<Empty<Bytes>>> {
+    let (response, fut) = upgrade::upgrade(&mut req)?;
+
+    tokio::spawn(async move {
+        if let Err(e) = handle_as_websocket(fut).await {
+            eprintln!("Error in websocket connection: {}", e);
+        }
+    });
+
+    Ok(response)
 }
